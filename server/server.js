daeaf8432a9b6f0219442eff8d122f0bc9c889b6
Braiton32 <braitononer32@gmail.com>
2025-12-09 00:20:34 -0600

    modificacion de archivos y testeo

--- server/server.js
+++ server/server.js
@@ -1,13 +1,21 @@
+const path = require('path');
 
-require('dotenv').config(); 
+require('dotenv').config({ path: '../.env' }); 
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors'); 
 
+
 const app = express();
+const staticPath = path.join(__dirname, '..', 'client'); 
+console.log('Ruta estática generada:', staticPath);      
+app.use(express.static(staticPath));
 
 app.use(cors()); 
 app.use(express.json()); 
+app.use(express.static(path.join(__dirname, '..', 'client')));
+
+
 
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('MongoDB Conectado con éxito.'))
@@ -16,6 +24,10 @@ mongoose.connect(process.env.MONGO_URI)
 const reportesRoutes = require('./routes/reportes');
 
 app.use('/api/reportes', reportesRoutes); 
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'client', 'index.html'));
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Servidor Express en funcionamiento: http://localhost:${PORT}`));
