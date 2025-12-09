2a3948257bfdc521ea73b39e58034c6c669533d7
Braiton32 <braitononer32@gmail.com>
2025-12-09 00:52:43 -0600

    Corregir ruta de reportes

--- server/routes/reportes.js
+++ server/routes/reportes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Reporte = require('../models/Reporte');
+const Reporte = require('../models/reporte');
 
 const multer = require('multer'); 
 const path = require('path'); 
